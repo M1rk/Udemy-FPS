1bc91af3f9b00061eb6af762d4ce2702c0cdbd44
M1rkWood <mizirekordia@gmail.com>
2019-05-02 23:50:16 +0200

    Третий коммит

---
+++ UdemyFPS/Source/UdemyFPS/UdemyFPS.Build.cs
@@ -0,0 +1,13 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class UdemyFPS : ModuleRules
+{
+	public UdemyFPS(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+	}
+}
