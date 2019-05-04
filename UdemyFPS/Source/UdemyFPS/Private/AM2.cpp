eb6e55a02adc39fcc6a08a6674f61af6d662b6c1
M1rkWood <mizirekordia@gmail.com>
2019-05-04 18:35:03 +0200

    тест на ссд

---
+++ UdemyFPS/Source/UdemyFPS/Private/AM2.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AM2.h"
+
+// Sets default values
+AAM2::AAM2()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AAM2::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AAM2::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
